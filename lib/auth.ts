e505ce41c8c9f7353f12e7853c2c3bed4d98c21d
DYSTOpyy <bcd.saltel@gmail.com>
2025-12-02 21:51:10 +0100

    feat: switching to prisma to predict future usage

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,23 @@
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
 import { nextCookies } from "better-auth/next-js";
 import { Pool } from "pg";
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
 export const auth = betterAuth({
-  database: new Pool({
-    connectionString: process.env.DATABASE_URL,
+  // database: new Pool({
+  //   connectionString: process.env.DATABASE_URL,
+  // }),
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
   }),
   emailAndPassword: {
     enabled: true,
