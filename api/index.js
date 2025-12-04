de507ff8d6adf3753095639f35d1ebaf00a8506e
Ecateln <clement.oge@proton.me>
2025-12-04 23:26:06 +0100

    changed admin default stats to make them json-compatible

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { v1: uuidv1 } = require('uuid');
 
 const admin_id = uuidv1();
-const users = { [admin_id]: { username: "admin", last_seen: new Date(), creation_date: get_date_obj(admin_id), currency: Infinity, cps: Infinity, multiplier: Infinity, purchases: [], flag: "INSASH{t1m3_b4s3d_Uu1D_4r3_pR3d1Ct4bl3}" } };
+const users = { [admin_id]: { username: "admin", last_seen: new Date(), creation_date: get_date_obj(admin_id), currency: 1e300, cps: 0, multiplier: 1, purchases: [], flag: "INSASH{t1m3_b4s3d_Uu1D_4r3_pR3d1Ct4bl3}" } };
 const PORT = 3000;
 
 const app = express();
